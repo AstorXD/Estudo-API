c6cc0d0d337e08ab386f6c48f4c946be77c0439c
AstorXD <79021539+AstorXD@users.noreply.github.com>
2025-01-31 21:29:11 -0300

    Primeira requisição de clima funcional, polir o restante

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 
+api_key = "b5f546a581ec7e5675a954a64b4e5ddb"
 countrycode = {}
 
 def org_countrycode():
@@ -27,20 +28,27 @@ def get_countrycode(countrycode, country):
         return countrycode[country]
     except Exception as error :
         return f"Erro ao buscar código do país: {error}"
+def translate_countryname(country):
 
-
-
-
+    with open("countrycodes.csv", newline="", encoding="utf-8") as arquivo_csv:
+        leitor = csv.reader(arquivo_csv)
+        for linha in leitor:
+            if len(linha) >= 2:
+                if linha[1] == country:
+                    try:
+                        return linha[0]
+                    except Exception as error:
+                        return f"Erro ao traduzir nome do país: {error}"
 
 #apenas testes
 org_countrycode()
 cidade = input("Insira o nome da cidade que deseja saber o clima: ")
 pais = input("Insira o pais dessa cidade: ")
+pais = str(translate_countryname(pais))
 c_code = get_countrycode(countrycode, pais)
-
-
-response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={cidade},{c_code}&limit=1&appid=b5f546a581ec7e5675a954a64b4e5ddb").json()
-
+response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={cidade},{c_code}&limit=1&appid={api_key}").json()
+lat_cidade = "Não foi possível recuperar a latitude"
+lon_cidade = "Não foi possível recuperar a longitude"
 if isinstance(response, list) and len(response) > 0:
     try:
         lat_cidade = response[0]["lat"]
@@ -48,9 +56,8 @@ if isinstance(response, list) and len(response) > 0:
     except Exception as error:
         lat_cidade = "Não foi possível recuperar a latitude"
         lon_cidade = "Não foi possível recuperar a longitude"
-
-
 print (response)
 print (lat_cidade)
 print (lon_cidade)
-
+weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat_cidade}&lon={lon_cidade}&appid={api_key}").json()
+print(weather)
