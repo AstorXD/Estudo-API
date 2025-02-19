0ebe9e486de8e283fb5eb496ec78ed31f8de6cd0
AstorXD <79021539+AstorXD@users.noreply.github.com>
2025-02-19 00:17:27 -0300

    Tratamento de erro backend

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import flask
 from flask_cors import CORS
+import waitress
 
 api_key = "b5f546a581ec7e5675a954a64b4e5ddb"
 countrycode = {}
@@ -27,12 +28,15 @@ def org_countrycode():
                 countrycode[chave.lower()] = valor
 def get_countrycode(countrycode, country):
     #tratamento de erro em pegar um código de país
+    if country is None:
+        print("Erro ao buscar código do país:!")
+        return None
     country = country.lower()
     try:
-        print(countrycode[country])
         return countrycode[country]
     except Exception as error :
-        return f"Erro ao buscar código do país: {error}"
+        print("Erro ao buscar código do país!")
+        return None
 def translate_countryname(country):
 
     with open("Backend/countrynames.csv", newline="", encoding="utf-8") as arquivo_csv2:
@@ -40,17 +44,19 @@ def translate_countryname(country):
         for linha in leitor:
             if len(linha) >= 2:
                 if linha[1].lower() == country.lower():
-                    try:
-                        print (linha[0])
                         return linha[0]
-                    except Exception as error:
-                        return f"Erro ao traduzir nome do país: {error}"
+        print ("Erro ao traduzir nome do país!")
+        return None
 def request_location(cidade, pais):
     global api_key
     pais = str(translate_countryname(pais))
     c_code = get_countrycode(countrycode, pais)
     try:
         response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={cidade},{c_code}&limit=1&appid={api_key}").json()
+        print(response)
+        if not response:
+            print("Nenhum resultado encontrado")
+            return None, None
         if isinstance(response, list) and len(response) > 0:
             lat_cidade = response[0]["lat"]
             lon_cidade = response[0]["lon"]
@@ -60,8 +66,10 @@ def request_location(cidade, pais):
 def request_weather(lat, lon):
     global api_key
     try:
-        weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&lang=pt_br").json()
-        return weather
+        weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&lang=pt_br")
+        if weather == None:
+            return flask.jsonify({"error": "Nenhum resultado encontrado para essas informações."}), 404
+        return weather.json()
     except Exception as error:
         return f"Erro ao fazer a requisição do clima: {error}"
 
@@ -71,18 +79,21 @@ org_countrycode()
 def response_weather():
     cidade = flask.request.args.get('cidade')
     pais = flask.request.args.get('pais')
-    if cidade is None:
+    if not cidade:
         return flask.jsonify({"error": "Cidade é uma informação obrigatória!"}), 400
-    if pais is None:
-        pais = ""
     try:
         lat_cidade, lon_cidade = request_location(cidade, pais)
+        if lat_cidade is None or lon_cidade is None:
+            return flask.jsonify({"error": "Nenhum resultado encontrado para essas informações."}), 404
         weather = request_weather(lat_cidade, lon_cidade)
+        if weather is None:
+            return flask.jsonify({"error": "Nenhum resultado encontrado para essas informações."}), 404
         return flask.jsonify(weather)
     except Exception as error:
         return flask.jsonify({"error": str(error)}), 400
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    waitress.serve(app, host="0.0.0.0", port=8080, threads=8)
 
 
 
